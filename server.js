58bad4146a0bdeadecd88afb23323052d784dfaa
seintun <seintun.edu@gmail.com>
2018-09-28 13:11:09 -0700

    updated bodyparser from var to const

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const port = 3000;
 const fs = require('fs');
 
